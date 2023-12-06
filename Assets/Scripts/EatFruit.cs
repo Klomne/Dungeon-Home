3dcfeca514455392bb45794fa17d4d48bbb8a22c
Klomne <149163490+Klomne@users.noreply.github.com>
2023-12-06 22:13:23 +0100

    Update 2023-12-06 22:13

--- Assets/Scripts/EatFruit.cs
+++ Assets/Scripts/EatFruit.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class EatFruit : Interactable
 {
-    public AudioClip NomAudio;
+    public AudioClip nomAudio;
     public override void OnInteraction()
     {
-        GameObject.Find("Ghost").GetComponent<AudioSource>().PlayOneShot(NomAudio);
+        GameObject.Find("Ghost").GetComponent<AudioSource>().PlayOneShot(nomAudio);
         Destroy(gameObject);
     }
 }
--- Assets/Scripts/Haunt.cs
+++ Assets/Scripts/Haunt.cs
@@ -8,14 +8,31 @@ public class Haunt : Interactable
 {
     Hover hover;
     bool haunted;
+    
+    //Sound Play
+    private AudioSource audioSource;
+    public AudioClip[] shoot;
+    private AudioClip shootClip;
+
     void Start()
     {
         hover = GetComponent<Hover>();
         hover.enabled = false;
+
+        //Sound Play
+        audioSource = gameObject.GetComponent<AudioSource>();
     }
 
     public override void OnInteraction()
     {
+        //Sound Play
+        int index = Random.Range(0, shoot.Length);
+        shootClip = shoot[index];
+
+        audioSource.clip = shootClip;
+        audioSource.Play();
+        
+        //Haunted Code
         haunted = !haunted;
         hover.enabled = haunted;
     }
